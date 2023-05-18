9ac7068075fef7068c1d6f2de564136cbc15eb09
Piyush-Jadhav2804 <71430828+Piyush-Jadhav2804@users.noreply.github.com>
2023-05-18 18:32:20 +0530

    fetch post from firebase and rendered on Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,43 @@
+import { useState, useEffect } from 'react';
+import { firestore } from '../firebase';
+import { Link } from 'react-router-dom';
+
 function Home() {
-  return <div className="">Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    firestore
+      .collection('posts')
+      .get()
+      .then((snapshot) => {
+        const posts = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+
+        console.log('posts', posts);
+        setPosts(posts);
+      });
+  }, []);
+  
+  return (
+    <div className="home">
+      <h1>Tech Blog</h1>
+      <div id="blog-by">Piyush</div>
+
+      {posts.map((post, index) => (
+        <div className="post" key={`post-${index}`}>
+          <Link to={`/post/${post.id}`}>
+            <h3>{post.title}</h3>
+          </Link>
+
+          <p>{post.subTitle}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
