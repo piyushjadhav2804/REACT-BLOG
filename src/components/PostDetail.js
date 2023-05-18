e4185f7b7271804debf81e88f0d4da7722c12057
Piyush-Jadhav2804 <71430828+Piyush-Jadhav2804@users.noreply.github.com>
2023-05-18 19:07:15 +0530

    completed the project and deployed on firebase

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+import { firestore } from '../firebase';
+import { useParams } from "react-router-dom";
+
 function PostDetail() {
-  return <div className="">PostDetail</div>;
+
+  const [post, setPost] = useState({});
+  const { postId } = useParams();
+
+  useEffect(() => {
+    firestore.collection('posts').doc(postId).get().then((snapshot) => {
+      console.log('snapshot', snapshot);
+      setPost(snapshot.data());
+    })
+  }, [postId]);
+
+  return <div className="post-detail">
+    <h1>{post.title}</h1>
+    <p>{post.content}</p>
+  </div>;
 }
 
 export default PostDetail;
